823bfd3a24d5439692230cb7f814e060b85cb2d0
Daniel <danielplimpias@gmail.com>
2023-06-08 13:44:31 -0400

    API DB process

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,9 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import org.ktorm.database.Database
-import org.ktorm.dsl.from
-import org.ktorm.dsl.insert
-import org.ktorm.dsl.select
+import org.ktorm.dsl.*
 
 fun main() {
 
@@ -34,11 +32,25 @@ fun main() {
     */
 
     //Leemos valores de la db
+    /*
     var notes = database.from(NotesEntity)
         .select()
     for (row in notes){
         println("${row[NotesEntity.id]}: ${row[NotesEntity.note]}")
+    }*/
+
+    //Update values
+    /*
+    database.update(NotesEntity){
+        set(it.note, "ByteSized")
+        where{it.id eq 4}
+    }*/
+
+    //Delete values
+    database.delete(NotesEntity){
+        it.id eq 2
     }
+
 
     //Esta parte va al final de la funcion main
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
