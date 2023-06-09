946455c644e73ab4fcc4945dd6d788a43e8db889
Daniel <danielplimpias@gmail.com>
2023-06-08 22:54:06 -0400

    Post inserting values to db

--- src/main/kotlin/com/example/routing/NotesRouter.kt
+++ src/main/kotlin/com/example/routing/NotesRouter.kt
@@ -3,21 +3,26 @@ package com.example.routing
 import com.example.db.DatabaseConnection
 import com.example.entities.NotesEntity
 import com.example.models.Note
+import com.example.models.NoteRequest
+import com.example.models.NoteResponse
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.dsl.from
+import org.ktorm.dsl.insert
 import org.ktorm.dsl.map
 import org.ktorm.dsl.select
 
-fun Application.notesRouter(){
+fun Application.notesRouter() {
 
     val db = DatabaseConnection.database
 
     routing {
 
         //GET show values from db
-        get("/notes"){
+        get("/notes") {
             val notes = db.from(NotesEntity).select()
                 .map {
                     val id = it[NotesEntity.id]
@@ -25,6 +30,29 @@ fun Application.notesRouter(){
                     Note(id ?: -1, note ?: "")
                 }
             call.respond(notes)
+        }
+
+        //POST inserting values to db
+        post("/notes") {
+            val request = call.receive<NoteRequest>()
+            val result = db.insert(NotesEntity) {
+                set(it.note, request.note)
+            }
+            if (result == 1) {
+                call.respond(
+                    HttpStatusCode.OK, NoteResponse(
+                        success = true,
+                        data = "Values has been successfully inserted"
+                    )
+                )
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest, NoteResponse(
+                        success = false,
+                        data = "Failed to insert values"
+                    )
+                )
+            }
         }
 
 
