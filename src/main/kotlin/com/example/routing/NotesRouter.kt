7ccc76e06766b2f54c7f57ad61c2914bc8c40d48
Daniel <danielplimpias@gmail.com>
2023-06-12 18:21:23 -0400

    DELETE note by id

--- src/main/kotlin/com/example/routing/NotesRouter.kt
+++ src/main/kotlin/com/example/routing/NotesRouter.kt
@@ -125,5 +125,35 @@ fun Application.notesRouter() {
             }
         }
 
+        //DELETE note by id
+        delete("/notes/{id}"){
+            //con esta linea recuperamos el id de la nota a deletear
+            val id = call.parameters["id"]?.toInt() ?: -1
+
+            //con este codigo verificamos el request si es valido o no, y de ser valido deleteamos la nota por id
+            //y respondemos al cliente OK o BadRequest
+            val rowsEffected = db.delete(NotesEntity){
+                it.id eq id
+            }
+            if (rowsEffected == 1) {
+                call.respond(
+                    HttpStatusCode.OK,
+                    NoteResponse(
+                        success = true,
+                        data = "Note successfully deleted"
+                    )
+                )
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data = "Note failed to delete"
+                    )
+                )
+            }
+        }
+
+
     }
 }
